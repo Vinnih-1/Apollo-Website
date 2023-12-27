6590feeb5e5b885c0237ad6cc66ca4b10e95dcb3
Vinnih-1 <vinifantal@hotmail.com>
2023-12-27 08:12:59 -0300

    add: table component

--- src/pages/dashboard/plans/Plans.tsx
+++ src/pages/dashboard/plans/Plans.tsx
@@ -114,8 +114,9 @@ export const Plans = () => {
                         setSelectService(service)
                         setOpen(true)
                       }}
-                      text="Detalhes"
-                    />
+                    >
+                      Detalhes
+                    </Table.Button>
                     <Modal.Root open={open}>
                       <Modal.Close onClick={() => setOpen(!open)} />
                       <Modal.Header title="Informações do Plano" />
@@ -155,100 +156,6 @@ export const Plans = () => {
                 ))}
             </Table.Content>
           </Table.Root>
-          {/* 
-          <div className="bg-zinc-100 max-w-5xl mx-auto rounded-lg shadow-xl mt-16 border border-zinc-200">
-            <div className="flex items-center justify-between p-8">
-              <h1 className="font-bold text-xl text-blue-600">
-                Lista de planos
-              </h1>
-            </div>
-            <div className="flex flex-col">
-              <div className="flex bg-zinc-200 py-2 px-4">
-                <span className="grow max-w-[33%]">ID do Plano</span>
-                <span className="grow hidden md:block max-w-[13%]">
-                  Service Key
-                </span>
-                <span className="grow hidden md:block max-w-[33%]">
-                  Expira em:
-                </span>
-              </div>
-              {services.length > 0 ? (
-                services.map((service, index) => (
-                  <div
-                    className="flex items-center bg-zinc-100 p-4"
-                    key={index}
-                  >
-                    <span className="grow text-zinc-700 text-sm max-w-[33%] overflow-hidden truncate">
-                      {service.id}
-                    </span>
-                    <span className="grow text-zinc-700 text-sm max-w-[13%] overflow-hidden truncate">
-                      {service.serviceKey}
-                    </span>
-                    <span className="grow text-zinc-700 text-sm max-w-[33%] overflow-hidden truncate">
-                      {service.expirateAt}
-                    </span>
-                    <button
-                      onClick={() => {
-                        setSelectService(service)
-                        setOpen(true)
-                      }}
-                      className="p-2 px-4 bg-blue-600 rounded-lg text-zinc-200 text-sm mx-auto"
-                    >
-                      Informações
-                    </button>
-                    <Modal.Root open={open}>
-                      <Modal.Close onClick={() => setOpen(!open)} />
-                      <Modal.Header title="Informações do Plano" />
-                      <Modal.Body>
-                        <Modal.Input
-                          label="Dono"
-                          title=""
-                          variant="outlined"
-                          disabled
-                          value={selectService?.owner}
-                        />
-                        <Modal.Input
-                          label="ID"
-                          title=""
-                          variant="outlined"
-                          disabled
-                          value={selectService?.id}
-                        />
-                        <Modal.Input
-                          label="Key"
-                          title=""
-                          variant="outlined"
-                          disabled
-                          value={selectService?.serviceKey}
-                        />
-                        <Modal.Footer>
-                          <span className="text-center text-sm text-red-600 text-light">
-                            {selectService?.expirateAt}
-                          </span>
-                          <div className="self-center">
-                            <Modal.Button>Suspender</Modal.Button>
-                          </div>
-                        </Modal.Footer>
-                      </Modal.Body>
-                    </Modal.Root>
-                  </div>
-                ))
-              ) : (
-                <div className="flex items-center bg-zinc-100 p-4">
-                  <span className="grow text-zinc-700 text-sm max-w-[33%] overflow-hidden truncate">
-                    -
-                  </span>
-                  <span className="grow text-zinc-700 text-sm max-w-[13%] overflow-hidden truncate">
-                    -
-                  </span>
-                  <span className="grow text-zinc-700 text-sm max-w-[33%] overflow-hidden truncate">
-                    -
-                  </span>
-                </div>
-              )}
-            </div>
-          </div>
-          */}
         </div>
       </div>
     </DashboardLayout>
