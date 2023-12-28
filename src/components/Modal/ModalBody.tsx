0113737859931890610fe8a7b3d451cb3351a8d9
Vinnih-1 <vinifantal@hotmail.com>
2023-12-28 04:26:31 -0300

    add: use tailwind merge

--- src/components/Modal/ModalBody.tsx
+++ src/components/Modal/ModalBody.tsx
@@ -1,9 +1,14 @@
-import { ReactNode } from 'react'
+import { ObjectHTMLAttributes, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-interface ModalBodyProps {
+interface ModalBodyProps extends ObjectHTMLAttributes<HTMLObjectElement> {
   children: ReactNode
 }
 
-export const ModalBody = ({ children }: ModalBodyProps) => {
-  return <div className="flex flex-col mt-4 gap-4">{children}</div>
+export const ModalBody = ({ children, ...rest }: ModalBodyProps) => {
+  return (
+    <div className={twMerge('flex flex-col mt-4 gap-4', rest.className)}>
+      {children}
+    </div>
+  )
 }
