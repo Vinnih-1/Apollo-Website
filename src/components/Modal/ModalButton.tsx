7eb60ab1e6e2dd409e3f4441ffac705ff503de5e
Vinnih-1 <vinifantal@hotmail.com>
2023-12-28 04:26:59 -0300

    add: use tailwind merge

--- src/components/Modal/ModalButton.tsx
+++ src/components/Modal/ModalButton.tsx
@@ -1,4 +1,5 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface ModalButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
@@ -8,7 +9,10 @@ export const ModalButton = ({ children, ...rest }: ModalButtonProps) => {
   return (
     <button
       {...rest}
-      className="py-2 px-4 bg-blue-600 text-zinc-200 text-sm rounded-lg"
+      className={twMerge(
+        'py-2 px-4 bg-blue-600 text-zinc-200 text-sm rounded-lg',
+        rest.className,
+      )}
     >
       {children}
     </button>
