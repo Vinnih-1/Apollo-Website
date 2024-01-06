5ff1810cfe0b50ea8ba2438491dc6ca40cdef22e
Vinnih-1 <vinifantal@hotmail.com>
2024-01-05 22:15:36 -0300

    fix: dashboard page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,42 +5,20 @@ import { Modal } from '@/components/Modal'
 import { Sidebar } from '@/components/Sidebar/Sidebar'
 import { Table } from '@/components/Table'
 import { useAuth } from '@/hooks/useAuth'
-import axios from 'axios'
+import { useService } from '@/hooks/useService'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import { DashboardLayout, PaymentProps, ServiceProps } from './DashboardLayout'
+import { DashboardLayout, PaymentProps } from './DashboardLayout'
 
 const Dashboard = () => {
   const validation = useAuth()
-  const [service, setService] = useState<ServiceProps | undefined>()
-  const [payments, setPayments] = useState<Array<PaymentProps>>([])
+  const service = useService()
   const [viewPayment, setViewPayment] = useState<PaymentProps>()
   const [open, setOpen] = useState(false)
 
   useEffect(() => {
     if (validation.token !== '') {
-      axios
-        .get(process.env.NEXT_PUBLIC_DASHBOARD_SERVICE as string, {
-          headers: {
-            Authorization: 'Bearer ' + validation.token,
-          },
-        })
-        .then((response) => {
-          const service = response.data as ServiceProps
-          setService(service)
-        })
-        .catch((error) => console.log(error))
-      axios
-        .get(process.env.NEXT_PUBLIC_DASHBOARD_PENDING_PAYMENTS as string, {
-          headers: {
-            Authorization: 'Bearer ' + validation.token,
-          },
-        })
-        .then((response) => {
-          const payments = response.data as Array<PaymentProps>
-          setPayments(payments)
-        })
-        .catch((error) => console.log(error))
+      service.updateServiceData(validation.token)
     }
   }, [validation])
 
@@ -76,35 +54,47 @@ const Dashboard = () => {
           <div className="flex flex-wrap items-center justify-center gap-4 max-w-5xl mx-auto mt-12 lg:h-auto lg:flex-row">
             <div className="bg-zinc-100 w-72 h-28 shadow-xl rounded-xl border border-zinc-200">
               <h1 className="ml-8 mt-4 text-zinc-400 text-sm font-light">
-                Total produtos
+                Compras em aberto
               </h1>
               <span className="ml-8 mt-4 text-blue-600 text-4xl font-bold">
-                {0}
+                R${' '}
+                {service.getServiceData
+                  ? service.getServiceData.payments
+                      .reduce(
+                        (total, payment) => total + payment.product.price,
+                        0,
+                      )
+                      .toFixed(2)
+                  : 0}
               </span>
               <span className="ml-8 mt-2 block text-zinc-400 text-xs font-light">
-                Há 3 min
+                Limitado em 20 pagamentos
               </span>
             </div>
             <div className="bg-zinc-100 w-72 h-28 shadow-xl rounded-xl border border-zinc-200">
               <h1 className="ml-8 mt-4 text-zinc-400 text-sm font-light">
-                Compradores
+                Produtos criados
               </h1>
               <span className="ml-8 mt-4 text-blue-600 text-4xl font-bold">
-                {0}
+                {service.getServiceData
+                  ? service.getServiceData.products.length
+                  : 0}
               </span>
               <span className="ml-8 mt-2 block text-zinc-400 text-xs font-light">
-                Há 3 min
+                Todos os produtos criados
               </span>
             </div>
             <div className="bg-zinc-100 w-72 h-28 shadow-xl rounded-xl border border-zinc-200">
               <h1 className="ml-8 mt-4 text-zinc-400 text-sm font-light">
-                Dinheiro movimentado
+                Cupons
               </h1>
               <span className="ml-8 mt-4 text-blue-600 text-4xl font-bold overflow-hidden">
-                R$ {service?.moneyMoved.toFixed(2)}
+                {service.getServiceData
+                  ? service.getServiceData.coupons.length
+                  : 0}
               </span>
               <span className="ml-8 mt-2 block text-zinc-400 text-xs font-light">
-                Há 3 min
+                Todos os cupons criados
               </span>
             </div>
           </div>
@@ -122,12 +112,12 @@ const Dashboard = () => {
                 <Table.Column text="Preço" />
                 <Table.Column text="Informações" />
               </Table.Header>
-              {payments.length > 0 &&
-                payments.map((payment, index) => (
+              {service.getServiceData?.payments &&
+                service.getServiceData?.payments.map((payment, index) => (
                   <Table.Data key={index}>
                     <Table.Row persist text={payment.payer} />
                     <Table.Row text={payment.id} />
-                    <Table.Row text={payment.price.toFixed(2)} />
+                    <Table.Row text={payment.product.price.toFixed(2)} />
                     <Table.Button
                       onClick={() => {
                         setViewPayment(payment)
@@ -137,57 +127,57 @@ const Dashboard = () => {
                     >
                       Detalhes
                     </Table.Button>
-                    <Modal.Root open={open}>
-                      <Modal.Close onClick={() => setOpen(!open)} />
-                      <Modal.Header title="Informações do Pagamento" />
-                      <Modal.Body>
-                        <Modal.Body className="!gap-0">
-                          <Modal.Text
-                            text="Comprador"
-                            className="text-center text-zinc-400 text-xs"
-                          />
-                          <Modal.Text
-                            text={viewPayment?.payer}
-                            className="text-center !font-light !text-sm !text-zinc-400 p-4 rounded-lg border border-zinc-200"
-                          />
-                        </Modal.Body>
-                        <Modal.Body className="!gap-0">
-                          <Modal.Text
-                            text="ID do Chat"
-                            className="text-center text-zinc-400 text-xs"
-                          />
-                          <Modal.Text
-                            text={viewPayment?.chatId}
-                            className="text-center !font-light !text-sm !text-zinc-400 p-4 rounded-lg border border-zinc-200"
-                          />
-                        </Modal.Body>
-                        <Modal.Text
-                          text={viewPayment?.paymentStatus}
-                          className="text-center !font-bold !text-lg !text-blue-600"
-                        />
-                        <Modal.Text
-                          text={'R$ ' + viewPayment?.price.toFixed(2)}
-                          className="!font-bold !text-4xl !text-blue-600 text-center"
-                        />
-                        <Modal.Footer>
-                          <Modal.Body className="!flex-row justify-center">
-                            <Modal.Button className="!bg-green-600">
-                              Aprovar
-                            </Modal.Button>
-                            <Modal.Button className="!bg-red-600">
-                              Cancelar
-                            </Modal.Button>
-                          </Modal.Body>
-                          <Modal.Text
-                            text={viewPayment?.id}
-                            className="text-sm !text-zinc-400 text-light"
-                          />
-                        </Modal.Footer>
-                      </Modal.Body>
-                    </Modal.Root>
                   </Table.Data>
                 ))}
             </Table.Content>
+            <Modal.Root open={open}>
+              <Modal.Close onClick={() => setOpen(!open)} />
+              <Modal.Header title="Informações do Produto" />
+              <Modal.Body>
+                <Modal.Body className="!gap-0">
+                  <Modal.Text
+                    text="Comprador"
+                    className="text-center text-zinc-400 text-xs"
+                  />
+                  <Modal.Text
+                    text={viewPayment?.payer}
+                    className="text-center !font-light !text-sm !text-zinc-400 p-4 rounded-lg border border-zinc-200"
+                  />
+                </Modal.Body>
+                <Modal.Body className="!gap-0">
+                  <Modal.Text
+                    text="ID do Chat"
+                    className="text-center !font-light !text-xs !text-zinc-400"
+                  />
+                  <Modal.Text
+                    text={viewPayment?.chatId}
+                    className="text-center !font-light !text-sm !text-zinc-400 p-4 rounded-lg border border-zinc-200"
+                  />
+                </Modal.Body>
+                <Modal.Text
+                  text={viewPayment?.paymentStatus}
+                  className="text-center text-zinc-400 text-xs"
+                />
+                <Modal.Text
+                  text={'R$ ' + viewPayment?.product.price.toFixed(2)}
+                  className="!font-bold !text-4xl !text-blue-600 text-center"
+                />
+                <Modal.Footer>
+                  <Modal.Body className="!flex-row justify-center">
+                    <Modal.Button className="!bg-green-600">
+                      Aprovar
+                    </Modal.Button>
+                    <Modal.Button className="!bg-red-600">
+                      Cancelar
+                    </Modal.Button>
+                  </Modal.Body>
+                  <Modal.Text
+                    text={viewPayment?.id}
+                    className="text-sm !text-zinc-400 text-light"
+                  />
+                </Modal.Footer>
+              </Modal.Body>
+            </Modal.Root>
           </Table.Root>
         </div>
       </div>
