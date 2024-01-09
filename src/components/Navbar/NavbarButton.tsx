a982cc99b1d303d7bf54856b9712a1ca549f2e2a
Vinnih-1 <vinifantal@hotmail.com>
2024-01-09 11:40:04 -0300

    fix: navbar button font size

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -8,7 +8,7 @@ export const NavbarButton = ({ text, ...rest }: NavbarButtonProps) => {
   return (
     <button
       {...rest}
-      className="bg-blue-800 text-white whitespace-nowrap rounded-lg px-0 py-0 w-0 lg:w-full lg:px-5 lg:py-2"
+      className="bg-blue-800 text-sm text-white whitespace-nowrap rounded-lg px-0 py-0 w-0 lg:w-full lg:px-5 lg:py-2"
     >
       {text}
     </button>
